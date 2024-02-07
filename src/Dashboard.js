419eb34b04c90bdbe88e3a2c75459d243c5285d3
ShitalNawal <shitalnawl17@gmail.com>
2024-02-07 15:28:29 +0530

    solve signIn issue and add dashboard

---
+++ src/Dashboard.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Dashboard() {
+  return (
+    <main>
+        <div className='text-center font-bold my-8'>Dashboard</div>
+        <div className='text-center '> 
+            Welcome to the Dashboard..!
+        </div>
+    </main>
+  )
+}
+
+export default Dashboard
